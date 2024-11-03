9b9e64ef0e57403c1da079d69c8cb2e294e753da
yhotta240 <yhotta240@icloud.com>
2024-11-03 12:35:36 +0900

    不要なコメントアウトされたコードを削除．メッセージパネルの開閉処理の変数名を変更し，コードの可読性を向上

--- popup/popup.js
+++ popup/popup.js
@@ -1,49 +1,66 @@
-
 // チェックボックスの状態の初期化
 let isEnabled = false;
-let blurIntensity = 5;
+let blurValue = 5;
 const blurEnabledElement = document.getElementById('blur-enabled');
 const blurElement = document.getElementById('blur-value');
+const panelButton = document.getElementById('panelButton');
+const messagePanel = document.getElementById('messagePanel');
+const messageDiv = document.getElementById('message');
+
 
 blurEnabledElement.addEventListener('change', (event) => {
-  isEnabled = event.target.checked; // チェックボックスの状態を取得
-  chrome.runtime.sendMessage({ isEnabled });
+  isEnabled = event.target.checked;
   chrome.storage.local.set({ isEnabled: isEnabled }, () => {
     messageOutput(dateTime(), isEnabled ? 'Easy Web Blurは有効になっています' : 'Easy Web Blurは無効になっています');
   });
 });
 
 blurElement.addEventListener('change', (event) => {
-  blurIntensity = event.target.value;
-  document.getElementById('blur-intensity').innerHTML = blurIntensity;
-  const message = `ぼかしの強さ ${blurIntensity}px が保存されました`;
+  blurValue = event.target.value;
+  document.getElementById('blur-intensity').innerHTML = blurValue;
+  const message = `ぼかしの強さ ${blurValue}px が保存されました`;
   saveSettings(dateTime(), message, event.target.value);
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('toggleButton');
-  const bottomPanel = document.getElementById('bottomPanel');
+chrome.storage.local.get(['settings', 'isEnabled'], (data) => {
 
-  toggleButton.addEventListener('click', function () {
-    // パネルが開いている場合は閉じる
-    if (bottomPanel.style.height === '150px') {
-      bottomPanel.style.height = '0';
-      toggleButton.textContent = 'メッセージパネルを開く';
-    }
-    // パネルが閉じている場合は開く
-    else {
-      bottomPanel.style.height = '150px'; // 必要に応じて高さを調整
-      toggleButton.textContent = 'メッセージパネルを閉じる';
+  let message;
+  if (blurElement) {
+    blurElement.value = data.settings ? data.settings.blurValue : 5;
+    document.getElementById('blur-intensity').innerHTML = blurElement.value;
+    message = `ぼかしの強さ ${blurElement.value}px`;
+  }
+  if (blurEnabledElement) {
+    isEnabled = data.isEnabled || false;
+    blurEnabledElement.checked = isEnabled;
+  }
+  messageOutput(dateTime(), isEnabled ? 'Easy Web Blurは有効になっています' : 'Easy Web Blurは無効になっています');
+  messageOutput(dateTime(), message);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  panelButton.addEventListener('click', function () {
+    const panelHeight = '170px';
+
+    if (messagePanel.style.height === panelHeight) {
+      // パネルが開いている場合は閉じる
+      messagePanel.style.height = '0';
+      panelButton.textContent   = 'メッセージパネルを開く';
+    } else {
+      // パネルが閉じている場合は開く
+      messagePanel.style.height = panelHeight;
+      panelButton.textContent   = 'メッセージパネルを閉じる';
     }
   });
 
-  blurIntensity = document.getElementById('blur-intensity').value;
-  // console.log(blurIntensity);
+  blurValue = document.getElementById('blur-intensity').value;
+  // console.log(blurValue);
 
   const storeLink = document.getElementById('store_link');
   if (storeLink) clickURL(storeLink);
-
   // manifest.jsonの情報を取得
   const manifestData = chrome.runtime.getManifest();
   // 各情報を要素に反映
@@ -60,37 +77,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-// 設定を保存する関数
+
 function saveSettings(datetime, message, value) {
-  // console.log(value);
   const settings = {
     blurValue: value,
   };
-
-  // chrome.storage.localに保存
   chrome.storage.local.set({ settings: settings }, () => {
     messageOutput(datetime, message);
   });
 
 }
 
+
 function clickURL(link) {
   const url = link.href ? link.href : link;
-  console.log(url);
   if (link instanceof HTMLElement) {
     link.addEventListener('click', (event) => {
-      event.preventDefault(); // デフォルトの動作を防止
-
+      event.preventDefault();
       chrome.tabs.create({ url });
-      // console.log("OK");
     });
   }
 }
 
 
-const messageDiv = document.getElementById('message');
 function messageOutput(datetime, message) {
-  messageDiv.innerHTML += '<p class="m-0">' + datetime + ' ' + message + '</p>'; // <p> タグで囲んで新しい行にする
+  messageDiv.innerHTML += '<p class="m-0">' + datetime + ' ' + message + '</p>';
 }
 
 document.getElementById('messageClearButton').addEventListener('click', () => {
@@ -98,7 +109,6 @@ document.getElementById('messageClearButton').addEventListener('click', () => {
 });
 
 function dateTime() {
-  // 現在の日付と時刻を取得
   const now = new Date();
   const year = now.getFullYear();           // 年
   const month = String(now.getMonth() + 1).padStart(2, '0'); // 月
@@ -106,27 +116,5 @@ function dateTime() {
   const hours = String(now.getHours()).padStart(2, '0');       // 時
   const minutes = String(now.getMinutes()).padStart(2, '0');   // 分
   const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}`;
-  // console.log(formattedDateTime);
   return formattedDateTime;
 }
-
-// 保存された設定を読み込む関数
-function loadSettings() {
-
-  chrome.storage.local.get(['settings', 'isEnabled'], (data) => {
-
-    let message;
-    if (blurElement) {
-      blurElement.value = data.settings ? data.settings.blurValue : 5;
-      document.getElementById('blur-intensity').innerHTML = blurElement.value;
-      message = `ぼかしの強さ ${blurElement.value}px`;
-    }
-    if (blurEnabledElement) {
-      isEnabled = data.isEnabled || false; // デフォルトはfalse
-      blurEnabledElement.checked = isEnabled; // チェックボックスの状態を設定
-    }
-    messageOutput(dateTime(), isEnabled ? 'Easy Web Blurは有効になっています' : 'Easy Web Blurは無効になっています');
-    messageOutput(dateTime(), message);
-  });
-}
-loadSettings();
